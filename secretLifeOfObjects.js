c83ff1ec5e44cbe4fa1b58d553e3d83b87e37d90
nei1d0r <superman4kryptonite@hotmail.com>
2021-02-21 12:35:39 +0000

    Update secretLifeOfObjects.js

--- secretLifeOfObjects.js
+++ secretLifeOfObjects.js
@@ -25,3 +25,48 @@ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 console.log(new Vec(3, 4).length);
 // → 5
+
+
+// Groups ---------------------------------------------------------------
+
+class Group {
+  // Your code here.
+  constructor(arr) {
+  	this.group = arr.length ? arr : []
+  }
+  
+  static from(arr){
+    const uniqueArray = []
+    arr.forEach((item) => {
+    	if (uniqueArray.indexOf(item) < 0) uniqueArray.push(item)
+    })
+    return new Group(uniqueArray);
+  }
+  
+  add(itemToAdd){
+    const index = (this.group).indexOf(itemToAdd)
+  	if (index < 0) this.group.push(itemToAdd)
+  }
+  
+  delete(itemToAdd){
+    const index = (this.group).indexOf(itemToAdd)
+    if (index > -1) {
+      this.group.splice(index, 1)
+    }
+  }
+  
+  has(itemToCheck){
+  	return this.group.indexOf(itemToCheck) !== -1 ? true : false
+  }
+}
+
+let group = Group.from([10, 20]);
+
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
