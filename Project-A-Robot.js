d84635f74be3963870542be937d19f4668a9cf69
nei1d0r <superman4kryptonite@hotmail.com>
2021-02-26 23:50:41 +0000

    Update Project-A-Robot.js

--- Project-A-Robot.js
+++ Project-A-Robot.js
@@ -37,3 +37,37 @@ function compareRobots(robot1, memory1, robot2, memory2) {
   console.log(`completed ${rounds} rounds in ${avgMoves(totalMoves1, rounds)} moves average`)
   console.log(`completed ${rounds} rounds in ${avgMoves(totalMoves2, rounds)} moves average`)
 }
+
+// ROBOT EFFICIENCY -----------------------------------------------------------
+// Their robot
+function goalOrientedRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)};
+}
+// My Robot
+function yourRobot({place, parcels}, route) {
+  if (route.length == 0) {
+    let pickUpRoutes = [];
+    let deliveryRoutes = [];
+    // we map the route for EACH parcel and add to appropriate array
+    parcels.forEach((parcel) => {
+      parcel.place != place
+        ? pickUpRoutes.push(findRoute(roadGraph, place, parcel.place))
+		    : deliveryRoutes.push(findRoute(roadGraph, place, parcel.address))
+    })
+
+    // we sort each array and use the 0th index (shortest route) to determine our direction
+    pickUpRoutes.sort((a, b) => a.length - b.length)
+    deliveryRoutes.sort((a, b) => a.length - b.length)
+	
+    route = pickUpRoutes.length == 0 ? deliveryRoutes[0] : pickUpRoutes[0]
+  }
+  return { direction: route[0], memory: route.slice(1) }
+}
