9b4b770ba7aa1a79bea366be2d3ea6f31f1b4144
nei1d0r <superman4kryptonite@hotmail.com>
2021-03-17 18:59:10 +0000

    Update document-object-model.js

--- document-object-model.js
+++ document-object-model.js
@@ -88,3 +88,35 @@ const byTagName = (node, tagName) => {
   console.log(byTagName(para, "span").length);
   // → 2
 </script>
+
+// THE CAT'S HAT ------------------------------------------------------------
+
+<style>body { min-height: 200px }</style>
+<img src="img/cat.png" id="cat" style="position: absolute">
+<img src="img/hat.png" id="hat" style="position: absolute">
+
+<script>
+  let cat = document.querySelector("#cat");
+  let hat = document.querySelector("#hat");
+
+  let angle = 0;
+  let lastTime = null;
+  function animate(time) {
+    if (lastTime != null) angle += (time - lastTime) * 0.001;
+    lastTime = time;
+    cat.style.top = (Math.sin(angle) * 40 + 40) + "px";
+    cat.style.left = (Math.cos(angle) * 200 + 230) + "px";
+
+    // Your extensions here.
+	  
+    // "This unit for measuring angles is called radians—a full circle is 
+    // 2π radians, similar to how it is 360 degrees when measuring in 
+    // degrees."
+    const hatAngle = angle + Math.PI
+    hat.style.top = (Math.sin(hatAngle) * 40 + 40) + "px";
+    hat.style.left = (Math.cos(hatAngle) * 200 + 230) + "px";
+
+    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
+</script>
